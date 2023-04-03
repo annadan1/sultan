66a0b01e126caf9c29c3dda01ef7fd4bd87a053b
annadan1 <anna_s92_@mail.ru>
2023-04-03 16:46:51 +0700

    Change webpack config

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store";
 import "./styles/styles.scss";
 import * as ReactDOMClient from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
 const init = () => {
   const container = document.getElementById("root") as HTMLElement;
   const root = ReactDOMClient.createRoot(container);
+
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </React.StrictMode>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -62,6 +62,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
     }),
+    new Dotenv(),
   ],
   output: {
     filename: "[name].js",
